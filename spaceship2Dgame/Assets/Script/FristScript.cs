3d71ec5c00263de451d16c26912be983e91d8609
bladodobird <109051069@live.asia.edu.tw>
2022-10-23 18:21:27 +0800

    測試程式

---
+++ spaceship2Dgame/Assets/Script/FristScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace YIZU
+{
+
+
+    public class FristScript : MonoBehaviour
+    {
+
+        #region
+        //喚醒事件
+        private void Awake()
+        {
+            print("你好");
+        }
+
+        //開始事件
+        private void Start()
+        {
+            
+        }
+        #endregion
+    }
+}
