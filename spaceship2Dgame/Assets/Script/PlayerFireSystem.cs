34294759249b2ad1f3c0e53455b759defc7bfc29
bladodobird <113589179+bladodobird@users.noreply.github.com>
2022-11-03 14:41:57 +0800

    繼續上船子彈程式

--- spaceship2Dgame/Assets/Script/PlayerFireSystem.cs
+++ spaceship2Dgame/Assets/Script/PlayerFireSystem.cs
@@ -7,6 +7,15 @@ namespace YIZU
     /// </summary>
     public class PlayerFireSystem : FireSystem
     {
+        // 偵測玩家輸入行為 : 鍵盤 滑鼠 觸控 搖桿
+        private void Update()
+        {
+            // 如果 玩家 按下空白建 生成 子彈
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SpawnBullet();
+            }
+        }
 
     }
 }
