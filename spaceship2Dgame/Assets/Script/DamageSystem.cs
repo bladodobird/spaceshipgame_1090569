a54dfeaf720a9b64ec7ce3998bc5ae093171717d
bladodobird <113589179+bladodobird@users.noreply.github.com>
2022-11-03 15:19:40 +0800

    Update DamageSystem.cs

--- spaceship2Dgame/Assets/Script/DamageSystem.cs
+++ spaceship2Dgame/Assets/Script/DamageSystem.cs
@@ -7,11 +7,23 @@ namespace YIZU
     {
         [SerializeField, Header("會造成傷害的目標")]
         private string nameTarget;
+        [SerializeField, Header("爆炸預置物")]
+        private GameObject prefabExplosion;
 
         // 碰撞開始事件 一次
         private void OnCollisionEnter2D(collision2D collision)
         {
-            print("碰撞 : " + collision.gameObject);
+            //print("碰撞 : " + collision.gameObject);
+
+            // 如果 碰到物件名稱 包含 敵機 就爆炸
+            if (collision.gameObject.name.Contain(nameTarget))
+            {
+                Instantiate(prefabExplosion, transform.position, transform.rotation);
+
+                // Destroy
+                // gameObject
+                Destroy(gameObject);
+            }
         }
 
         // 碰撞離開事件 一次
